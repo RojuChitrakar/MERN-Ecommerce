c9ece840c99237dc0ab87e206db8a90cf8f23b66
RojuChitrakar <rojuchit00@gmail.com>
2026-03-27 10:30:39 +0530

    Connected to Database

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log("Connection Failed", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("API RUNNING...");
+});
+
+const PORT = process.env.PORT || 3200;
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});

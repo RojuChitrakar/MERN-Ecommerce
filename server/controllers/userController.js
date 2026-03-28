9660645b849aecd7668d5ee9d7011e97492d074e
RojuChitrakar <rojuchit00@gmail.com>
2026-03-28 11:25:39 +0530

    Added Login API

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -50,3 +50,29 @@ export const registerUser = async (req, res) => {
   }
 };
 
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Please fill all the fields." });
+    }
+
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+      res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};

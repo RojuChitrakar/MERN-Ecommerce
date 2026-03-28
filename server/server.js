8782c4de81f3fd3c29fafb6c9a2608e96f2e1dbd
RojuChitrakar <rojuchit00@gmail.com>
2026-03-28 10:23:34 +0530

    defined user Schema and password hashing

--- server/server.js
+++ server/server.js
@@ -10,8 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose
-  .connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
   })

564301ad8291d13b05ec520d980c87cd3a397071
RojuChitrakar <rojuchit00@gmail.com>
2026-03-28 15:08:14 +0530

    added admin middleware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { registerUser,loginUser } from '../controllers/userController.js';
+import { admin, protect } from '../middleware/authMiddleware.js';
 
 const router=express.Router();
 
 router.post("/register",registerUser);
 
 router.post("/login",loginUser);
+
+router.post("/product", protect, admin,createProduct);
 
 export default router;

e3d26e7acfd9d1cfd8495e5a2574baa2f166d31b
RojuChitrakar <rojuchit00@gmail.com>
2026-03-30 19:58:23 +0530

    added wishlist feature and made api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -78,3 +78,60 @@ export const loginUser = async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 };
+
+export const addToWishlist=async (req,res)=>{
+  try{
+    const user=await User.findById(req.user._id);
+    if(user){
+      const productId=req.params.id;
+
+      if(!user.wishlist.find(item => item.toString() === productId)){
+        user.wishlist.push(productId);
+        await user.save();
+      }
+
+      res.status(200).json(user.wishlist);
+    }else{
+      res.status(404).json({message:"User not found"});
+    }
+  }catch(error){
+    console.error(error);
+    res.status(500).json({message:"Server Error"})
+    
+  }
+};
+
+export const removeFromWishlist=async (req,res)=>{
+  try{
+    const user=await User.findById(req.user._id);
+
+    if(user){
+      user.wishlist=user.wishlist.filter(
+        (item)=>item.toString() !==req.params.id
+      );
+      await user.save();
+    }else{
+      res.status(404).json({message:"User not found"});
+    }
+  }catch(error){
+    console.error(error);
+    res.status(500).json({message:"Server Error"})
+    
+  }
+};
+
+export const getWishlist=async(req,res)=>{
+  try{
+    const user= await User.findById(req.user._id).populate("wishlist");
+    if(user){
+      res.status(200).json(user.wishlist);
+    }
+    else{
+      res.status(404).json({message:"User not found"});
+    }
+  }catch(error){
+    console.error(error);
+    res.status(500).json({message:"Server Error"});
+    
+  }
+};

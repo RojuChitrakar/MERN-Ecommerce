598db7d16acb1dd7d45e3d43e7d63dd6f7fa0a13
RojuChitrakar <rojuchit00@gmail.com>
2026-03-31 10:28:47 +0530

    Added Review Routes

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -90,3 +90,43 @@ export const deleteProduct = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+export const createProductReview= async (req,res)=>{
+  try{
+    const {rating, comment}=req.body;
+    const product=await Product.findById(req.params.id);
+    if(product){
+      const alreadyReviewed=product.reviews.find(
+        (r)=>r.user.toString()===req.user._id.toString()
+      );
+
+      if(alreadyReviewed){
+        return res.status(400).json({message:"Product already reviewed"});
+      }
+
+      const review={
+        name:req.user.name,
+        rating:Number(rating),
+        comment,
+        user:req.user._id
+      };
+
+      product.reviews.push(review);
+
+      product.numReviews=product.reviews.length;
+
+      product.rating=product.reviews.reduce((acc,item)=> item.rating+acc,0)/product.reviews.length;
+
+      await product.save();
+
+      res.status(201).json({message:"Review Added"});
+    }else{
+      res.status(404).json({message:"Product not found"});
+    }
+
+  }catch(error){
+    console.error(error);
+    res.status(500).json({message:"Server Error"});
+    
+  }
+};
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,14 +1,20 @@
 import exppress from 'express';
 import {getProducts, getProductById, createProduct, updateProduct, deleteProduct} from "../controllers/productController.js";
 import { admin, protect } from '../middleware/authMiddleware.js';
+import { createProductReview } from '../controllers/productController.js';
 
 const router=exppress.Router();
 
 router.get("/",getProducts);
+
 router.get("/:id",getProductById);
 
 router.post("/",protect,admin,createProduct);
+
 router.put("/:id", protect, admin, updateProduct);
+
 router.delete("/:id",protect, admin, deleteProduct);
+
+router.post("/:id/reviews", protect , createProductReview);
 
 export default router;

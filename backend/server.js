77f9df394413da3d9f2916782d8a4d4121b54fa6
RojuChitrakar <rojuchit00@gmail.com>
2026-04-18 11:20:31 +0530

    moved dist in backend

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ dotenv.config(); // ✅ still okay to keep (good practice)
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
-
+import path from "path";
+import { fileURLToPath } from "url";
 import productRoutes from "./routes/productRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -14,6 +15,8 @@ const startServer = async () => {
   await connectDB();
 
   const app = express();
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
 
   app.use(express.json());
 
@@ -28,7 +31,17 @@ const startServer = async () => {
   app.use("/api/auth", authRoutes);
   app.use("/api/users", userRoutes);
   app.use("/api/orders", orderRoutes);
+  const frontendPath = path.join(__dirname, "dist");
+  app.use(express.static(frontendPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(frontendPath, "index.html"));
+  });
 
+  // ERROR HANDLER
+  app.use((err, req, res, next) => {
+    console.error("GLOBAL ERROR:", err);
+    res.status(500).json({ message: err.message });
+  });
   app.get("/", (req, res) => {
     res.send("API Running...");
   });
